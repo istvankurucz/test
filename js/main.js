0b3349612b15a458a207e46fc976c511db739c5a
István Kurucz <kurucz.isti63@gmail.com>
2024-06-16 17:05:26 +0200

    Test project initialization

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+	loadTemplate("headerContainer", "/templates/header.html");
+});
+
+async function loadTemplate(containerId, templateUrl) {
+	try {
+		const res = await fetch(templateUrl);
+		if (!res.ok) {
+			throw new Error("Network response was not ok " + res.statusText);
+		}
+
+		const template = await res.text();
+		document.getElementById(containerId).innerHTML = template;
+	} catch (e) {
+		console.error("There has been a problem with your fetch operation:", e);
+	}
+}
